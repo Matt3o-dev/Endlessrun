742dac70ea80927729c754dc3d699cfc8e373c0a
Matteo <matteomorvillo@gmail.com>
2022-04-08 22:35:37 +0200

    piccole modifiche ed aggiunta della lattina (che si può raccogliere)

---
+++ Assets/Inventory/Scripts/Mono/Cursor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+}
